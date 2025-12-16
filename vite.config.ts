72ebddbabf585b5b1dc98f9a1ced416ca8acc33b
Aaqid Masoodi <aaqidmasoodi@icloud.com>
2025-12-16 13:55:31 +0000

    fix: explicit COOP/COEP headers middleware for localhost

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,19 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp")
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin")
+          next()
+        })
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
