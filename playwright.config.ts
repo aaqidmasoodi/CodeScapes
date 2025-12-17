88a5e43c57f0074f4c0bfe23add3834c804d6bbb
Aaqid Masoodi <aaqidmasoodi@icloud.com>
2025-12-17 13:49:58 +0000

    chore(test): remove slowMo completely

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,9 +30,6 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    launchOptions: {
-      slowMo: process.env.CI ? 0 : 1000,
-    },
   },
 
   /* Configure projects for major browsers */
