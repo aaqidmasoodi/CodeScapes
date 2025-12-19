a9ab816165db8a662fa578d7f8e1a60ca5be4138
Aaqid Masoodi <aaqidmasoodi@icloud.com>
2025-12-19 00:16:00 +0000

    fix(build): remove unused variable in vite config to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     {
       name: "configure-response-headers",
       configureServer: (server) => {
-        server.middlewares.use((_req, res, next) => {
+        server.middlewares.use((_req, _res, next) => {
           // res.setHeader("Cross-Origin-Embedder-Policy", "require-corp")
           // res.setHeader("Cross-Origin-Opener-Policy", "same-origin")
           next()
